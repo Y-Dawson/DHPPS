7f339e14e230677601b86cbc90ae5e14642fb4e2
Watremons <z2331355763@sina.com>
2021-01-08 16:50:53 +0800

    去除模型引用

--- DHPPSWeb/backend/urls.py
+++ DHPPSWeb/backend/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from backend import views
-from backend.views import LinearRegression
 
 router = DefaultRouter()
 router.register('AccountInfos', views.AccountViewSet)
